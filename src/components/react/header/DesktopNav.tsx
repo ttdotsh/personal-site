396bfdbc0717cafcef6836ef430df668e0a8a00f
timTonelli <102175985+timTonelli@users.noreply.github.com>
2023-03-25 16:30:25 -0400

    create desktop nav v1

--- src/components/react/header/DesktopNav.tsx
+++ src/components/react/header/DesktopNav.tsx
@@ -1,29 +1,21 @@
-interface Link {
-  text: string
-  href: string
-}
+import type { NavItem } from "@types"
 
-interface Props {
-  navItems: Link[]
+interface DesktopNavProps {
+  navItems: NavItem[]
   className?: string
 }
 
 /**
  * @requires client:load
  */
-export function DesktopNav(props: Props) {
+export function DesktopNav({ navItems, className }: DesktopNavProps) {
   return (
-    <nav {...props}>
+    <nav className={className}>
       <ul className="flex rounded-full bg-white/70 px-3 text-sm font-medium shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:bg-zinc-800/70 dark:ring-zinc-50/10">
-        {props.navItems.map(({ text, href }) => (
-          <li>
-            <a
-              className="block px-3 py-2 transition hover:text-teal-500 dark:hover:text-teal-400"
-              href={href}
-            >
-              {text}
-            </a>
-          </li>
+        {navItems.map(({ text, href }, i) => (
+          <NavItem key={i} href={href}>
+            {text}
+          </NavItem>
         ))}
       </ul>
     </nav>
@@ -31,19 +23,23 @@ export function DesktopNav(props: Props) {
 }
 
 interface NavItemProps {
-  link: Link
+  href: string
+  children: React.ReactNode
 }
 
-function NavItem({ link }: NavItemProps) {
-  const isActive = link.href === window.location.pathname
+function NavItem({ href, children }: NavItemProps) {
+  /**
+   * @todo add highlighting active nav item
+   * const isActive = href === window.location.pathname
+   */
 
   return (
     <li>
       <a
-        className="px-3 py-2 transition hover:text-teal-500 dark:hover:text-teal-400"
-        href={link.href}
+        className="block px-3 py-2 transition hover:text-teal-500 dark:hover:text-teal-400"
+        href={href}
       >
-        {link.text}
+        {children}
       </a>
     </li>
   )
