555fbaafc5eb46810976c8f2d037ef34de96905f
timTonelli <102175985+timTonelli@users.noreply.github.com>
2023-03-30 18:20:51 -0400

    npm install sharp

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
+import { defineConfig } from "astro/config"
+import tailwind from "@astrojs/tailwind"
+import react from "@astrojs/react"
 
-import image from "@astrojs/image";
+import image from "@astrojs/image"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), image()]
-});
+  integrations: [
+    tailwind(),
+    react(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+  ],
+})
