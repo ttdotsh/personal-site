64fe722513641947360888f36a67244decaeaf7a
timTonelli <102175985+timTonelli@users.noreply.github.com>
2023-04-07 23:05:19 -0400

    edit content and spacing

--- src/components/react/work-timeline/content.tsx
+++ src/components/react/work-timeline/content.tsx
@@ -5,7 +5,7 @@ export const timeline = [
     content: "Started working at",
     target: "VIA Science",
     detail:
-      "Currently, I'm working as a full-stack developer at Via Science, where I build and maintain frontend apps in Angular and React, backend API’s and microservices with Python and Celery, and deployment pipelines with tools like Docker, Kubernetes, and AWS.",
+      "Currently, I'm working as a full-stack developer at Via Science, where I build and maintain frontend apps in Angular and React, backend API’s and microservices with Python, Docker, Kubernetes and Celery, and deployment pipelines with tools like Github Actions, Terraform and AWS.",
     date: "Sep 2022",
     Icon: WorkIcon,
     iconBackground: "bg-blue-500",
@@ -22,7 +22,7 @@ export const timeline = [
   {
     content: "Career Transition",
     detail:
-      "I had been really unhappy in my professional life for a long time, and eventually had enough. I had always had interest in technology, but had never considered that it could be for me. After trying a few free coding resources, I was hooked and I quit my job in order to pursue software development full time. During this time, I attended a bootcamp to provide some structure and acceleration to the learning process.",
+      "I had been unhappy with my career path for a while, and eventually I decided it was time to make a move. I wanted to move into a career where I could have more impact in what I was doing and more agency over my own growth. After validating my interest in coding, I decided this was the direction I wanted to go in. During this time, I attended a bootcamp to provide some structure and acceleration to the learning process. What I love about this industry is that it encourages and demands constant learning. As someone who loves to discover new concepts, practice new skills and take on new challenges, I cannot imagine a more perfect space to exist in.",
     date: "Dec 2021",
     Icon: RocketIcon,
     iconBackground: "bg-orange-500",
