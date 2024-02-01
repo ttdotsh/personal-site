2502b39f3aced31cd598843a314fce96ccb90491
timTonelli <102175985+timTonelli@users.noreply.github.com>
2024-01-31 20:47:44 -0500

    simplify theme toggle

--- src/components/react/header/ThemeToggle.tsx
+++ src/components/react/header/ThemeToggle.tsx
@@ -13,26 +13,20 @@ export function ThemeToggle() {
       className="group rounded-full p-2 dark:text-zinc-50/50"
       onClick={toggleTheme}
     >
-      <ThemeIcon />
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className={cn(
+          "h-6 w-6 fill-none transition",
+          "group-hover:fill-current group-hover:text-orange-400",
+        )}
+      >
+        <LightPath className="dark:hidden" />
+        <DarkPath className="hidden dark:block" />
+      </svg>
     </button>
-  )
-}
-
-function ThemeIcon() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      strokeWidth={1.5}
-      stroke="currentColor"
-      className={cn(
-        "h-6 w-6 fill-none transition",
-        "group-hover:fill-current group-hover:text-orange-400",
-      )}
-    >
-      <LightPath className="dark:hidden" />
-      <DarkPath className="hidden dark:block" />
-    </svg>
   )
 }
 
